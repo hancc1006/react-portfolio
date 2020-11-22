039974d106ac08e4e4e6b48482bd521f0271c291
Crystal Han <hancc1006@gmail.com>
2020-11-21 20:11:09 -0800

    cleanup

--- src/components/Info/homedescription.js
+++ src/components/Info/homedescription.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./home.css";
-import Profile from "../Profile/Profile";
-import Nav from "../Navbar/Navbar";
 import {Link} from "react-router-dom";
 function HomeDescription(){
     return(
